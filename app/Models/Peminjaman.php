4b9c08e3f4d6c97aaa7e476cce1cfc74ca44a9c6
syahla1503 <oss566637@gmail.com>
2026-04-01 23:36:45 -0700

    menambahkan peminjaman.php

---
+++ app/Models/Peminjaman.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Peminjaman extends Model
+{
+    use HasFactory;
+
+    protected $table = 'peminjamans';
+    protected $fillable = ['user_id', 'book_id', 'tanggal_pinjam', 'tanggal_kembali', 'status'];
+
+   public function user() {
+    return $this->belongsTo(User::class, 'user_id');
+}
+
+public function book() {
+    return $this->belongsTo(Book::class, 'book_id');
+}
+}
